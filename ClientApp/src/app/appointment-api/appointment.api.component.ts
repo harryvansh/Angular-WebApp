c7bbd38f390ff5f0cb08ade9a01b86ad13b415f3
Dylan Brigham <dbrigha1@uwyo.edu>
2018-07-27 23:31:24 -0600

    add get customer to appointment ts file

--- ClientApp/src/app/appointment-api/appointment.api.component.ts
+++ ClientApp/src/app/appointment-api/appointment.api.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: "appointment-api",
     templateUrl: "./appointment.api.component.html"
 })
-export class AppointmentApiComponent{
+export class AppointmentApiComponent implements OnInit {
+    private readonly _http: HttpClient;
+    public readonly _baseUrl: string;
+    public appointments: AppointmentModel[];
+    
 
+    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl:string) {
+        this._http = http;
+        this._baseUrl = baseUrl;
+    }
+
+    ngOnInit() {
+        this.getAllAppointments();
+    }
+    private getAllAppointments() {
+        let options: {
+            headers: { 'Content-Type': 'application/json' }
+        }
+
+        this._http.get<AppointmentModel[]>(this._baseUrl + 'api/Customers/Customer', options).subscribe(result => this.appointments = result);
+
+    }
+}
+
+interface AppointmentModel {
+    CustomerId: number,
+    firstName: string,
+    MiddleName: string,
+    lastName: string,
+    displayName: string,
+    PreferredName: string,
+    Birthday: Date,
+    EmployeeId: number
 }
 
